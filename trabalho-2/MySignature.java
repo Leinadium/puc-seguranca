c81239c3b75dea068a365fd89de7f0bd4b47bb37
Leinadium <daniel.sch.guima@gmail.com>
2023-04-03 08:34:47 -0300

    Trabalho 5 abril

--- trabalho-2/MySignature.java
+++ trabalho-2/MySignature.java
@@ -1,8 +1,100 @@
 /*
-*  Daniel Guimarães - 1910462
-*
-*/
+ *  Daniel Guimarães - 1910462
+ *  Luiz Fellipe Augusto - 1711256
+ */
+
+import java.security.*;
+import java.util.Arrays;
+import java.util.Objects;
+import javax.crypto.*;
 
 public class MySignature {
+    // algoritmo de hash
+    // chave privada
+    // chave publica
+
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+    private MessageDigest messageDigest;
+    private Cipher cipher;
+    private byte[] document;
+
+    /**
+     * Cria uma instancia do MySignature configurado com os argumentos
+     * @param args o algoritmo de hash e de chave
+     * @return a instancia
+     */
+    public static MySignature getInstance(String args) throws Exception {
+        String[] argsList = args.split("with");
+        if (argsList.length != 2) {
+            System.out.println(argsList[0]);
+            System.out.println(argsList[1]);
+            throw new Exception("Invalid arg");
+        }
+        MySignature instance = new MySignature();
+
+        // salvando os algs
+        String hashAlg = argsList[0];
+        String keyAlg = argsList[1];
+
+        if (!Objects.equals(hashAlg, "MD5") &&
+                !Objects.equals(hashAlg, "SHA1") &&
+                !Objects.equals(hashAlg, "SHA256") &&
+                !Objects.equals(hashAlg, "SHA512")) {
+            throw new Exception("algoritmo de hash invalido");
+        }
+
+        if (!Objects.equals(keyAlg, "RSA")) {
+            throw new Exception("algoritmo de criptografia de chave invalido");
+        }
+
+        String transformation = keyAlg + "/ECB/PKCS1Padding";
+
+        // criando os objetos
+        instance.messageDigest = MessageDigest.getInstance(hashAlg);
+        instance.cipher = Cipher.getInstance(transformation);
+
+        return instance;
+    }
+
+    /**
+     * Initializa a assinatura com a chave privada
+     * @param pk chave privada
+     */
+    public void initSign(PrivateKey pk) {
+        this.privateKey = pk;
+    }
+
+    public void update(byte[] document) {
+        this.document = document;
+    }
+
+    public byte[] sign() throws Exception {
+        // fiz o hash
+        byte[] digest = this.messageDigest.digest(this.document);
+
+        // passando pela chave privada
+        this.cipher.init(Cipher.ENCRYPT_MODE, this.privateKey);
+        byte[] cifrado = this.cipher.doFinal(digest);
+
+        this.document = null;
+        return cifrado;
+    }
+
+    public void initVerify(PublicKey pk) {
+        this.publicKey = pk;
+        return;
+    }
+
+    public boolean verify(byte[] assinatura) throws Exception {
+        // fiz o hash
+        byte[] digest = this.messageDigest.digest(this.document);
+
+        // decriptar usando a public key!
+        this.cipher.init(Cipher.DECRYPT_MODE, this.publicKey);
+        byte[] decriptado = this.cipher.doFinal(assinatura);
+
+        return Arrays.equals(decriptado, digest);
+    }
 
 }
--- trabalho-2/MySignatureTest.java
+++ trabalho-2/MySignatureTest.java
@@ -1,3 +1,67 @@
+/*
+ *  Daniel Guimarães - 1910462
+ *  Luiz Fellipe Augusto - 1711256
+ */
+
+import java.security.*;
+//
+// este exemplo utililiza facilidades para a geracao e verificacao
+// de assinatura digital
 public class MySignatureTest {
 
+    private static String toHex(byte[] meusBytes) {
+        // converte o signature para hexadecimal
+        StringBuilder buf = new StringBuilder();
+        for (byte meusByte : meusBytes) {
+            String hex = Integer.toHexString(0x0100 + (meusByte & 0x00FF)).substring(1);
+            buf.append(hex.length() < 2 ? "0" : "").append(hex);
+        }
+        return buf.toString();
+    }
+
+    public static void main (String[] args) throws Exception {
+        if (args.length != 2 ) {
+            System.err.println("Uso: java MySignatureTest padraoAssinatura documento");
+            System.exit(1);
+        }
+        // argumentos
+        String padraoAssinatura = args[0];
+        String[] algoritmos = padraoAssinatura.split("with");
+        byte[] documento = args[0].getBytes("UTF8");
+
+        // gerando as chaves
+        // poderia usar RSA direto pois todas as opcoes do MySignature utilizando RSA
+
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(algoritmos[1]);
+        keyGen.initialize(2048);
+
+        System.out.println("Gerando par de chaves...");
+        KeyPair chaves = keyGen.generateKeyPair();
+        System.out.println("\nChave publica: " + toHex(chaves.getPublic().getEncoded()));
+        System.out.println("\nChave privada: " + toHex(chaves.getPrivate().getEncoded()));
+
+        // criando o objeto MySignature e configurando
+        System.out.println("\n\nConfigurando MySignature para geração...");
+        MySignature sig = MySignature.getInstance(padraoAssinatura);
+        sig.initSign(chaves.getPrivate());
+        sig.update(documento);
+
+        // pegando a assinatura e o digest
+        byte[] signature = sig.sign();
+        MessageDigest md = MessageDigest.getInstance(algoritmos[0]);
+        System.out.println("\n\nDigest: " + toHex(md.digest(documento)));
+        System.out.println("\nAssinatura: " + toHex(signature));
+
+        // verificando a assinatura
+        System.out.println("\n\nConfigurando MySignature para verificação...");
+        sig.initVerify(chaves.getPublic());
+        sig.update(documento);
+
+        System.out.println("Validando assinatura: ");
+        if (sig.verify(signature)) {
+            System.out.println("Assinatura válida! :)");
+        } else {
+            System.out.println("Assinatura inválida :(");
+        }
+    }
 }
