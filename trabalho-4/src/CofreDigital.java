58fb0f38ec15f3de87014d6fbb77521ea4a08e59
Leinadium <daniel.sch.guima@gmail.com>
2023-05-08 17:03:48 -0300

    Criando classe para interface
    Signed-off-by: Leinadium <daniel.sch.guima@gmail.com>

--- trabalho-4/src/CofreDigital.java
+++ trabalho-4/src/CofreDigital.java
@@ -1,13 +1,17 @@
 import diretorio.Diretorio;
 import diretorio.Restaurador;
 import basedados.Conexao;
+import terminal.InterfaceTerminal;
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
 public class CofreDigital {
     public static void main(String[] args) throws Exception {
-        Conexao conexao = Conexao.getInstance();
+        // Conexao conexao = Conexao.getInstance();
+
+        InterfaceTerminal.mostrarInicio();
+
 
         Diretorio dir = new Diretorio("/home/leinadium/puc/puc-seguranca/trabalho-4/Pacote-T4/Files");
 
--- trabalho-4/src/terminal/InterfaceTerminal.java
+++ trabalho-4/src/terminal/InterfaceTerminal.java
@@ -1,4 +1,13 @@
 package terminal;
 
 public class InterfaceTerminal {
+    public static void mostrarInicio() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        System.out.println("Bem vindo ao Cofre Digital!");
+        System.out.println("Escolha uma opção:");
+        System.out.println("1 - Criar novo diretório");
+        System.out.println("2 - Abrir diretório existente");
+        System.out.println("3 - Sair");
+    }
 }
