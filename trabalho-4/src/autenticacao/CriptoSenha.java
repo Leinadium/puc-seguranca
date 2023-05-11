60443b5dd2882f1ddb8ec58cc5d5c3c45008d571
Luiz Fellipe <lfsca08@gmail.com>
2023-05-11 11:23:01 -0300

    criptsenha

---
+++ trabalho-4/src/autenticacao/CriptoSenha.java
@@ -0,0 +1,9 @@
+package autenticacao;
+
+import org.bouncycastle.crypto.generators.OpenBSDBCrypt;
+
+public class CriptoSenha {
+    public static byte[] encripta(String senhaDig) {
+        // OpenBSDBCrypt.generate()
+    }
+}
